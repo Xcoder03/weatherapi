a87bdd66dd7e99d8eb172119732e101ca5aa0b0b
Xcoder <chindaebube03@gmail.com>
2022-12-12 13:42:33 +0100

    Finished design the application

--- src/App.js
+++ src/App.js
@@ -4,16 +4,35 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const url = 'https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=32fa52ae0be6a8074e9cf7731c9c4521'
-  return (
+ // const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=32fa52ae0be6a8074e9cf7731c9c4521`
+  const [data, setData] = useState('')
+ 
+ 
+ return (
     <div className="app">
-      <div className="container">
-          <input type="text" className="" ></input>
+       <input type="text" className="" placeholder="Enter Location" ></input>  
+       <div className="city">
           <h2>City</h2>
-          <div className="location">
+          <p>Temp &#8457;</p>
+          <p>Clouds</p>
+       </div>
 
-           </div>
-        </div>   
+       <div className="container-props">
+          <div className="props">
+            <p>Temp &#8457;</p>
+            <p>Feels Like</p>
+          </div>
+
+          <div className="props">
+            <p>Temp &#8457;</p>
+            <p>Humidity</p>
+          </div>
+
+          <div className="props">
+            <p>Temp &#8457;</p>
+            <p>Wind</p>
+          </div>
+       </div>
     </div>
   );
 }
